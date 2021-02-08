e191ac5bf60954735877a886aac82232a87c6aeb
BryanPeens <peensbryan1@gmail.com>
2021-02-08 22:50:30 +0200

    fixes

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -10,7 +10,7 @@ const Cards = ( {data: { confirmed, recovered, deaths, lastUpdate } }) => {
     if(!confirmed){
         return 'Loading...';
     }
-    
+    // .
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
